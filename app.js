842a0f395c790128f1570005a63aec7d4bea25a8
Hardik Ajmeriya <hardikpt95@gmail.com>
2025-07-22 12:46:15 +0530

    Services Module Done

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/services", (req, res) => {
+  res.render("services");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 const PORT = process.env.PORT || 3000;
